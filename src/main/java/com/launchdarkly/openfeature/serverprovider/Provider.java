c5154c21aa9a17e4756aa63801cf1d6fa5c4ee2e
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-02-16 16:35:56 -0800

    Implement configuration and add some additional documentation to the provider.

--- src/main/java/com/launchdarkly/openfeature/serverprovider/Provider.java
+++ src/main/java/com/launchdarkly/openfeature/serverprovider/Provider.java
@@ -1,14 +1,32 @@
 package com.launchdarkly.openfeature.serverprovider;
 
+import com.launchdarkly.logging.LDLogAdapter;
 import com.launchdarkly.logging.LDLogger;
 import com.launchdarkly.sdk.EvaluationDetail;
 import com.launchdarkly.sdk.LDValue;
 import com.launchdarkly.sdk.server.LDClient;
+import com.launchdarkly.sdk.server.subsystems.LoggingConfiguration;
 import dev.openfeature.sdk.*;
 
-
+/**
+ * An OpenFeature {@link FeatureProvider} which enables the use of the LaunchDarkly Server-Side SDK for Java
+ * with OpenFeature.
+ * <pre><code>
+ *import dev.openfeature.sdk.OpenFeatureAPI;
+ *import com.launchdarkly.sdk.server.LDClient;
+ *
+ *public class Main {
+ *  public static void main(String[] args) {
+ *    LDClient ldClient = new LDClient("my-sdk-key");
+ *    OpenFeatureAPI.getInstance().setProvider(new Provider(ldClient));
+ *
+ *    // Refer to OpenFeature documentation for getting a client and performing evaluations.
+ *  }
+ *}
+ * </code></pre>
+ */
 public class Provider implements FeatureProvider {
-    private class ProviderMetaData implements Metadata {
+    private static final class ProviderMetaData implements Metadata {
         @Override
         public String getName() {
             return "LaunchDarkly.OpenFeature.ServerProvider";
@@ -24,13 +42,39 @@ public class Provider implements FeatureProvider {
 
     private LDClient client;
 
-    public Provider(LDClient client) {
+    /**
+     * Create a provider with the given LaunchDarkly client and provider configuration.
+     * <pre><code>
+     * // Using the provider with a custom log level.
+     * new Provider(ldclient, ProviderConfiguration
+     *     .builder()
+     *     .logging(Components.logging().level(LDLogLevel.INFO)
+     *     .build());
+     * </code></pre>
+     *
+     * @param client A {@link LDClient} instance.
+     * @param config Configuration for the provider.
+     */
+    public Provider(LDClient client, ProviderConfiguration config) {
         this.client = client;
-        logger = LDLogger.none(); // TODO: Implement config.
+        LoggingConfiguration loggingConfig = config.getLoggingConfigurationFactory().build(null);
+        LDLogAdapter adapter = loggingConfig.getLogAdapter();
+        logger = LDLogger.withAdapter(adapter, loggingConfig.getBaseLoggerName());
 
         evaluationContextConverter = new EvaluationContextConverter(logger);
         evaluationDetailConverter = new EvaluationDetailConverter(logger);
         valueConverter = new ValueConverter(logger);
+    }
+
+    /**
+     * Create a provider with the given LaunchDarkly client.
+     * <p>
+     * The provider will be created with default configuration.
+     *
+     * @param client A {@link LDClient} instance.
+     */
+    public Provider(LDClient client) {
+        this(client, ProviderConfiguration.builder().build());
     }
 
 
@@ -75,7 +119,7 @@ public class Provider implements FeatureProvider {
     public ProviderEvaluation<Value> getObjectEvaluation(String key, Value defaultValue, EvaluationContext ctx) {
         EvaluationDetail<LDValue> detail
                 = this.client.jsonValueVariationDetail(
-                        key, evaluationContextConverter.toLdContext(ctx), valueConverter.toLdValue(defaultValue));
+                key, evaluationContextConverter.toLdContext(ctx), valueConverter.toLdValue(defaultValue));
 
         return evaluationDetailConverter.toEvaluationDetailsLdValue(detail);
     }
