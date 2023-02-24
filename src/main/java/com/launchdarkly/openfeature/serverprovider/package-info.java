1ef85212bdffa4ccd1c71442bc34b13f23c3bae9
LaunchDarklyReleaseBot <launchdarklyreleasebot@launchdarkly.com>
2023-02-24 23:09:42 +0000

    Updating documentation to version 0.1.0

--- src/main/java/com/launchdarkly/openfeature/serverprovider/package-info.java
+++
@@ -1,9 +0,0 @@
-/**
- * Main package for the LaunchDarkly OpenFeature provider for the Server-Side SDK for Java, containing the provider
- * and configuration classes.
- * <p>
- * You will most often use {@link com.launchdarkly.openfeature.serverprovider.Provider} (the provider) and
- * {@link com.launchdarkly.openfeature.serverprovider.ProviderConfiguration} (configuration options for the provider).
- * <p>
- */
-package com.launchdarkly.openfeature.serverprovider;
