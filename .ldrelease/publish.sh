9aa9b1dce0c2eaee955d6f1fa934276901dbcd62
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2023-02-24 13:15:18 -0800

    Apply shellscript formatting.

--- .ldrelease/publish.sh
+++ .ldrelease/publish.sh
@@ -5,7 +5,13 @@ set -ue
 # Publish to Sonatype
 echo "Publishing to Sonatype"
 if [[ -n "${LD_RELEASE_IS_PRERELEASE}" ]]; then
-  ./gradlew publishToSonatype || { echo "Gradle publish/release failed" >&2; exit 1; }	
+  ./gradlew publishToSonatype || {
+    echo "Gradle publish/release failed" >&2
+    exit 1
+  }
 else
-  ./gradlew publishToSonatype closeAndReleaseRepository || { echo "Gradle publish/release failed" >&2; exit 1; }
+  ./gradlew publishToSonatype closeAndReleaseRepository || {
+    echo "Gradle publish/release failed" >&2
+    exit 1
+  }
 fi
