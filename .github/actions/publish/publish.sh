0166522d2b27271260d7b765e850de58fafdcf3f
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-06-05 14:00:08 -0700

    Cleanup.

--- .github/actions/publish/publish.sh
+++ .github/actions/publish/publish.sh
@@ -8,13 +8,13 @@ else
     echo "Publishing to Sonatype"
     if [ "${LD_RELEASE_IS_PRERELEASE}" == "true" ]; then
         echo "PRERELEASE"
-        ${WORKSPACE_PATH}/gradlew publishToSonatype -p ${WORKSPACE_PATH} -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_KEY_PASSPHRASE}" -Psigning.secretKeyRingFile="${SIGNING_SECRET_KEY_RING_FILE}" -PsonatypeUsername="${SONATYPE_USER_NAME}" -PsonatypePassword="${SONATYPE_PASSWORD}" || {
+        ./gradlew publishToSonatype -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_KEY_PASSPHRASE}" -Psigning.secretKeyRingFile="${SIGNING_SECRET_KEY_RING_FILE}" -PsonatypeUsername="${SONATYPE_USER_NAME}" -PsonatypePassword="${SONATYPE_PASSWORD}" || {
             echo "Gradle publish/release failed" >&2
             exit 1
         }
     else
         echo "RELEASE"
-        ${WORKSPACE_PATH}/gradlew publishToSonatype closeAndReleaseRepository -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_KEY_PASSPHRASE}" -Psigning.secretKeyRingFile="${SIGNING_SECRET_KEY_RING_FILE}" -PsonatypeUsername="${SONATYPE_USER_NAME}" -PsonatypePassword="${SONATYPE_PASSWORD}" || {
+        ./gradlew publishToSonatype closeAndReleaseRepository -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_KEY_PASSPHRASE}" -Psigning.secretKeyRingFile="${SIGNING_SECRET_KEY_RING_FILE}" -PsonatypeUsername="${SONATYPE_USER_NAME}" -PsonatypePassword="${SONATYPE_PASSWORD}" || {
             echo "Gradle publish/release failed" >&2
             exit 1
         }
