16b328c29f1cf4b8e4bd193e9d3794c489f5968e
Ryan Lamb <4955475+kinyoklion@users.noreply.github.com>
2024-06-05 15:23:50 -0700

    fix: Include java version for build when publishing. (#25)

--- .github/actions/publish/publish.sh
+++ .github/actions/publish/publish.sh
@@ -2,17 +2,21 @@
 
 set -ue
 
-echo "Publishing to Sonatype"
-if [ "${LD_RELEASE_IS_PRERELEASE}" == "true" ]; then
-  echo "PRERELEASE"
-  ./gradlew publishToSonatype -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.secretKeyRingFile="${SIGNING_SECRET_KEY_RING_FILE}" -PossrhUsername="${SONATYPE_USER_NAME}" -PossrhPassword="${SONATYPE_PASSWORD}" || {
-    echo "Gradle publish/release faile" >&2
-    exit 1
-  }
+if $LD_RELEASE_IS_DRYRUN ; then
+  echo "Doing a dry run of publishing."
 else
-  echo "RELEASE"
-  ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository || {
-    echo "Gradle publish/release failed" >&2
-    exit 1
-  }
+    echo "Publishing to Sonatype"
+    if [ "${LD_RELEASE_IS_PRERELEASE}" == "true" ]; then
+        echo "PRERELEASE"
+        ./gradlew publishToSonatype -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_KEY_PASSPHRASE}" -Psigning.secretKeyRingFile="${SIGNING_SECRET_KEY_RING_FILE}" -PsonatypeUsername="${SONATYPE_USER_NAME}" -PsonatypePassword="${SONATYPE_PASSWORD}" || {
+            echo "Gradle publish/release failed" >&2
+            exit 1
+        }
+    else
+        echo "RELEASE"
+        ./gradlew publishToSonatype closeAndReleaseSonatypeStagingRepository -Psigning.keyId="${SIGNING_KEY_ID}" -Psigning.password="${SIGNING_KEY_PASSPHRASE}" -Psigning.secretKeyRingFile="${SIGNING_SECRET_KEY_RING_FILE}" -PsonatypeUsername="${SONATYPE_USER_NAME}" -PsonatypePassword="${SONATYPE_PASSWORD}" || {
+            echo "Gradle publish/release failed" >&2
+            exit 1
+        }
+    fi
 fi
